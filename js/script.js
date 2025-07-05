5ecbd11b57b54b9c4bf723e1d81088f3c222b39e
Hifzaan123 <mohdhifzaanansari2004@gmail.com>
2025-07-05 12:32:07 +0530

    make profile size big

--- js/script.js
+++ js/script.js
@@ -111,16 +111,23 @@ document.addEventListener("DOMContentLoaded", async () => {
       const { user } = await res.json();
 
      // Replace buttons with profile icon (SVG only)
-const profileBtn = document.createElement("a");
+   const profileBtn = document.createElement("a");
 profileBtn.href = "#";
-profileBtn.className = "btn btn-outline";
+profileBtn.style.border = "none";
+profileBtn.style.padding = "0";
+profileBtn.style.background = "transparent";
+profileBtn.style.cursor = "pointer";
+
 profileBtn.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="22" height="22">
-    <path d="M256 73.825a182.175 182.175 0 1 0 182.18 182.18A182.177 182.177 0 0 0 256 73.825zm0 71.833a55.05 55.05 0 1 1-55.054 55.046A55.046 55.046 0 0 1 256 145.658zm.52 208.723h-80.852c0-54.255 29.522-73.573 48.885-90.906a65.68 65.68 0 0 0 62.885 0c19.363 17.333 48.885 36.651 48.885 90.906z" data-name="Profile"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="60" height="60 " fill="#333">
+    <path d="M256 73.825a182.175 182.175 0 1 0 182.18 182.18A182.177 182.177 0 0 0 256 73.825zm0 71.833a55.05 55.05 0 1 1-55.054 55.046A55.046 55.046 0 0 1 256 145.658zm.52 208.723h-80.852c0-54.255 29.522-73.573 48.885-90.906a65.68 65.68 0 0 0 62.885 0c19.363 17.333 48.885 36.651 48.885 90.906z"/>
   </svg>
 `;
+
 ctaContainer.innerHTML = "";
 ctaContainer.appendChild(profileBtn);
+
+
 
       // Change hero button
       heroBtn.textContent = "➡ Continue to Dashboard";
